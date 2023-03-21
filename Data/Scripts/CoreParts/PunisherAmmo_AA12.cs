7af2969fce71cf5ff3f51969cdcba57c7a6e1fc0
Muzz <dalymitchell11@gmail.com>
2023-03-21 16:01:47 -0600

    orange actually

--- Data/Scripts/CoreParts/PunisherAmmo_AA12.cs
+++ Data/Scripts/CoreParts/PunisherAmmo_AA12.cs
@@ -734,7 +734,7 @@ namespace Scripts
                         Enable = true,
                         Length = 12f, //
                         Width = 0.25f, //
-                        Color = Color(red: 15, green: 12.5f, blue: 2.5f, alpha: 1), // RBG 255 is Neon Glowing, 100 is Quite Bright.
+                        Color = Color(red: 25, green: 10f, blue: 1f, alpha: 1), // RBG 255 is Neon Glowing, 100 is Quite Bright.
                         VisualFadeStart = 0, // Number of ticks the weapon has been firing before projectiles begin to fade their color
                         VisualFadeEnd = 0, // How many ticks after fade began before it will be invisible.
                         Textures = new[] {// WeaponLaser, ProjectileTrailLine, WarpBubble, etc..
