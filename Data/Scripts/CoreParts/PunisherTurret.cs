5c2379da6e8be0185b9d627b210fbbfd4bacdf0f
MuzzMuzz <dalymitchell11@gmail.com>
2023-06-13 01:30:59 -0600

    [mystery git noises]

--- Data/Scripts/CoreParts/PunisherTurret.cs
+++ Data/Scripts/CoreParts/PunisherTurret.cs
@@ -128,7 +128,7 @@ namespace Scripts {
                 },
                 Loading = new LoadingDef
                 {
-                    RateOfFire = 250, // Set this to 3600 for beam weapons. This is how fast your Gun fires.
+                    RateOfFire = 480, // Set this to 3600 for beam weapons. This is how fast your Gun fires.
                     BarrelsPerShot = 1, // How many muzzles will fire a projectile per fire event.
                     TrajectilesPerBarrel = 4, // Number of projectiles per muzzle per fire event.
                     SkipBarrels = 0, // Number of muzzles to skip after each fire event.
