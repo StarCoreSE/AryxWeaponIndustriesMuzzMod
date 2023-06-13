729724e69b6a2eb220a3a627af5fcdfcafb17d21
InvalidArgument3 <InvalidArgument3@gmail.com>
2023-06-12 23:57:08 -0500

    Ares +100mw

--- Data/Scripts/CoreParts/AresGauss_Ammo.cs
+++ Data/Scripts/CoreParts/AresGauss_Ammo.cs
@@ -33,7 +33,7 @@ namespace Scripts
             AmmoMagazine = "GaussAmmoMagDef", // SubtypeId of physical ammo magazine. Use "Energy" for weapons without physical ammo.
             AmmoRound = "Classic Ares", // Name of ammo in terminal, should be different for each ammo type used by the same weapon. Is used by Shrapnel.
             HybridRound = true, // Use both a physical ammo magazine and energy per shot.
-            EnergyCost = 0.0018334f, // Scaler for energy per shot (EnergyCost * BaseDamage * (RateOfFire / 3600) * BarrelsPerShot * TrajectilesPerBarrel). Uses EffectStrength instead of BaseDamage if EWAR.
+            EnergyCost = 0.015114f, // Scaler for energy per shot (EnergyCost * BaseDamage * (RateOfFire / 3600) * BarrelsPerShot * TrajectilesPerBarrel). Uses EffectStrength instead of BaseDamage if EWAR.
             BaseDamage = 660000f, // Direct damage; one steel plate is worth 100.
             Mass = 2500f, // In kilograms; how much force the impact will apply to the target.
             Health = 0, // How much damage the proj ectile can take from other projectiles (base of 1 per hit) before dying; 0 disables this and makes the projectile untargetable.
